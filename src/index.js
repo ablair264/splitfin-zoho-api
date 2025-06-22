22bd2146dfac71c384e2b895e5dc0bd13b7eda0d
ablair264 <blair@hotmail.co.uk>
2025-06-22 14:08:24 +0100

    Initial commit - Three Counties booking backend

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import cors from 'cors';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
-import { verifyFirebaseToken } from './routes/auth.js';
 
 // Import routes
 import webhookRoutes from './routes/webhooks.js';
